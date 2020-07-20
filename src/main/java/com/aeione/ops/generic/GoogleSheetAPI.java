a98b9130399e2bd9cce65980e954d52d15da7f18
kirthanaAuto <kirthana.subbanna@qualitrix.com>
2020-07-20 16:24:12 +0530

    Removed setting constant PortNumber from GoogleSheetAPI.java

--- src/main/java/com/aeione/ops/generic/GoogleSheetAPI.java
+++ src/main/java/com/aeione/ops/generic/GoogleSheetAPI.java
@@ -105,7 +105,7 @@ public class GoogleSheetAPI {
 //         receiver = new LocalServerReceiver.Builder().setCallbackPath("http://aeione.com:8090/Callback").setPort(8090).build();
 //        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
 
-      LocalServerReceiver receiver = new LocalServerReceiver.Builder().setHost("127.0.0.1").setPort(8090).build();
+      LocalServerReceiver receiver = new LocalServerReceiver.Builder().setHost("127.0.0.1").build();
      // LocalServerReceiver receiver = new LocalServerReceiver.Builder().setHost("jenkins.opsplt.com").setPort(8090).build();
 
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
